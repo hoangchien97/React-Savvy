b1b9f2f8c4f801c16321ac8d651d12f9aaa99aa4
Chien Hoang <chien.hoang@savvycomsoftware.com>
2020-02-28 17:08:53 +0700

    Composing Components

--- src/index.js
+++ src/index.js
@@ -1,24 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import HelloWorld from "./HelloWorld";
+import App from "./App";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
-// C1 : trong component
-
-// function Welcome(props) {
-//   return <h1>Hello, {props.name}</h1>;
-// }
-
-// const element = <Welcome name="Sara" />;
-// ReactDOM.render(element, document.getElementById("root"));
-
-// C2 pass prop by export/import component
-
-ReactDOM.render(
-  <HelloWorld title="I'm a Viet Nam" />,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
