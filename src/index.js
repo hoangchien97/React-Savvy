4b60c9f44870c167478683738c1dd4edfe3833c7
Chien Hoang <chien.hoang@savvycomsoftware.com>
2020-02-28 16:54:51 +0700

    pass props components by import/export file

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import HelloWorld from "./HelloWorld";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
-function tick() {
-  const element = (
-    <div>
-      <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-    </div>
-  );
-  ReactDOM.render(element, document.getElementById("root"));
-}
+// C1 : trong component
 
-setInterval(tick, 1000);
+// function Welcome(props) {
+//   return <h1>Hello, {props.name}</h1>;
+// }
+
+// const element = <Welcome name="Sara" />;
+// ReactDOM.render(element, document.getElementById("root"));
+
+// C2 pass prop by export/import component
+
+ReactDOM.render(
+  <HelloWorld title="I'm a Viet Nam" />,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
