63b525ec42496e677f9f5fbf77b4a3ac268db719
Chien Hoang <hoangchien@savvycomsoftware.com>
2020-03-02 10:03:12 +0700

    using state

--- src/index.js
+++ src/index.js
@@ -1,27 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import Clock from "./Clock";
 import * as serviceWorker from "./serviceWorker";
-
-// 1, Replace this.props.date with this.state.date in the render() method:
-// 2, Add a class constructor that assigns the initial this.state:
-// 3, Remove the date prop from the <Clock /> element:
-
-
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { date: new Date() };
-  }
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    );
-  }
-}
 
 ReactDOM.render(<Clock />, document.getElementById("root"));
 
