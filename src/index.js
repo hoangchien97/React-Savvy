9136197152f02756c4f0519bdc3b9d25e6d18750
Chien Hoang <hoangchien@savvycomsoftware.com>
2020-03-02 11:25:42 +0700

    If with Logical && Operator

--- src/index.js
+++ src/index.js
@@ -2,8 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
-import Login from "./Login";
 
-ReactDOM.render(<Login />, document.getElementById("root"));
+function Mailbox(props) {
+  const unreadMessages = props.unreadMessages;
+  return (
+    <div>
+      <h1>Hello!</h1>
+      {unreadMessages.length > 0 && (
+        <h2>You have {unreadMessages.length} unread messages.</h2>
+      )}
+    </div>
+  );
+}
+
+const messages = ["React", "Re: React", "Re:Re: React"];
+ReactDOM.render(
+  <Mailbox unreadMessages={messages} />,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
