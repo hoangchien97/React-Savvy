1674f5ca94390b14175ca3b772c208df14860975
ChienHD <hoangchien@savvycomsoftware.com>
2020-03-02 17:04:42 +0700

    focus input text

--- src/index.js
+++ src/index.js
@@ -2,46 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
-import FilterableProductTable from "./Thingking";
+import CustomTextInput from "./CustomTextInput";
 
-
-const PRODUCTS = [
-  {
-    category: "Sporting Goods",
-    price: "$49.99",
-    stocked: true,
-    name: "Football"
-  },
-  {
-    category: "Sporting Goods",
-    price: "$9.99",
-    stocked: true,
-    name: "Baseball"
-  },
-  {
-    category: "Sporting Goods",
-    price: "$29.99",
-    stocked: false,
-    name: "Basketball"
-  },
-  {
-    category: "Electronics",
-    price: "$99.99",
-    stocked: true,
-    name: "iPod Touch"
-  },
-  {
-    category: "Electronics",
-    price: "$399.99",
-    stocked: false,
-    name: "iPhone 5"
-  },
-  { category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7" }
-];
-
-ReactDOM.render(
-  <FilterableProductTable products={PRODUCTS} />,
-  document.getElementById("root")
-);
+ReactDOM.render(<CustomTextInput />, document.getElementById("root"));
 
 serviceWorker.unregister();
