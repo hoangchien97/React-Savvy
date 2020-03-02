e5ec8c2b99b9f28a50960f474720b09d58baa75f
Chien Hoang <hoangchien@savvycomsoftware.com>
2020-03-02 10:53:18 +0700

    conditional

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import Toggle from "./Toggle";
-import LoggingButton from "./LoggingButton";
 import * as serviceWorker from "./serviceWorker";
 
-// ReactDOM.render(<Toggle />, document.getElementById("root"));
-ReactDOM.render(<LoggingButton />, document.getElementById("root"));
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>;
+}
+
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>;
+}
+
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UserGreeting />;
+  }
+  return <GuestGreeting />;
+}
+
+ReactDOM.render(
+  <Greeting isLoggedIn={false} />,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
