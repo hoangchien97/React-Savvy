fc9c7bac1bcf77831ace18acb6c1eeae0994ca66
ChienHD <hoangchien@savvycomsoftware.com>
2020-03-02 17:53:47 +0700

    onBlue

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
-// import CustomTextInput from "./CustomTextInput";
-import OuterClick from "./OuterClick";
+import Blur from "./Blue";
 
-// ReactDOM.render(<CustomTextInput />, document.getElementById("root"));
-ReactDOM.render(<OuterClick />, document.getElementById("root"));
+ReactDOM.render(<Blur />, document.getElementById("root"));
 
 serviceWorker.unregister();
