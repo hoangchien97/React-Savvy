b1dd226b4a5e62a693b2224d5da72bc50fcfb7d8
ChienHD <hoangchien@savvycomsoftware.com>
2020-03-02 17:17:28 +0700

    toggle click

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
-import CustomTextInput from "./CustomTextInput";
+// import CustomTextInput from "./CustomTextInput";
+import OuterClick from "./OuterClick";
 
-ReactDOM.render(<CustomTextInput />, document.getElementById("root"));
+// ReactDOM.render(<CustomTextInput />, document.getElementById("root"));
+ReactDOM.render(<OuterClick />, document.getElementById("root"));
 
 serviceWorker.unregister();
