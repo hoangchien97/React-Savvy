013b3d4b2ef2f2b5699b40b5d9f2aa3476c6f11d
Chien Hoang <hoangchien@savvycomsoftware.com>
2020-03-02 10:42:03 +0700

    using event func ES6 .

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Toggle from "./Toggle";
+// import Toggle from "./Toggle";
+import LoggingButton from "./LoggingButton";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<Toggle />, document.getElementById("root"));
+// ReactDOM.render(<Toggle />, document.getElementById("root"));
+ReactDOM.render(<LoggingButton />, document.getElementById("root"));
 
 serviceWorker.unregister();
