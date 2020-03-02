bda8e050d0900930f51f1075a3d986080570de55
ChienHD <hoangchien@savvycomsoftware.com>
2020-03-02 16:29:12 +0700

    last project thinking react

--- src/index.js
+++ src/index.js
@@ -2,8 +2,46 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
-import Calculator from "./Calculator";
+import FilterableProductTable from "./Thingking";
 
-ReactDOM.render(<Calculator />, document.getElementById("root"));
+
+const PRODUCTS = [
+  {
+    category: "Sporting Goods",
+    price: "$49.99",
+    stocked: true,
+    name: "Football"
+  },
+  {
+    category: "Sporting Goods",
+    price: "$9.99",
+    stocked: true,
+    name: "Baseball"
+  },
+  {
+    category: "Sporting Goods",
+    price: "$29.99",
+    stocked: false,
+    name: "Basketball"
+  },
+  {
+    category: "Electronics",
+    price: "$99.99",
+    stocked: true,
+    name: "iPod Touch"
+  },
+  {
+    category: "Electronics",
+    price: "$399.99",
+    stocked: false,
+    name: "iPhone 5"
+  },
+  { category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7" }
+];
+
+ReactDOM.render(
+  <FilterableProductTable products={PRODUCTS} />,
+  document.getElementById("root")
+);
 
 serviceWorker.unregister();
